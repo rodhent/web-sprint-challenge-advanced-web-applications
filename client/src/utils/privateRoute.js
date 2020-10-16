d5dd69d0a839be171083fcb4901a49b01bef4ff2
Rod Hentringer <67014030+rodhent@users.noreply.github.com>
2020-10-16 14:59:56 -0400

    utils done

--- client/src/utils/privateRoute.js
+++ client/src/utils/privateRoute.js
@@ -1,20 +1,19 @@
-import React, { Component } from "react"
-import {Route, Redirect} from "react-router-dom"
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
     return (
-        <Route 
-        {...rest}
-        render={props =>
-            localStorage.getItem('token') ? (
-                <Component {...props} />
-            ) : (
-                <Redirect to="/login" />
-            )
-        }
-        
+        <Route  
+            {...rest}
+            render={props =>
+                localStorage.getItem('token') ? (
+                    <Component {...props} />
+                ) : (
+                    <Redirect to="/login" />
+                )
+            }
         />
     )
 }
 
-export default PrivateRoute
+export default PrivateRoute;
