ad0e04c33e6f39685eb74db7efd522f3817c0af4
Rod Hentringer <67014030+rodhent@users.noreply.github.com>
2020-10-16 14:59:36 -0400

    components done

--- client/src/components/BubblePage.test.js
+++ client/src/components/BubblePage.test.js
@@ -1,7 +1,21 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, wait } from "@testing-library/react";
 import BubblePage from "./BubblePage";
 
-test("Fetches data and renders the bubbles", () => {
-  // Finish this test
+test("bubbles", () => {
+  render(<BubblePage />)
+});
+
+test("bubbles", async () => {
+  const token = "ahuBHejkJJiMDhmODZhZi0zaeLTQ4ZfeaseOGZgesai1jZWYgrTA07i73Gebhu98"
+  localStorage.setItem('token', token);
+  const { getByText } = render(<BubblePage />)
+
+  await wait (() => {
+    getByText(/bisque/i);
+  });
+  const bubble = screen.getByText(/bisque/i);
+
+  expect(bubble).toBeInTheDocument();
+
 });
